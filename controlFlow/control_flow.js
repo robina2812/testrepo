23332b7a505cf87a199e5e93e46aa50fb63e99fc
robina2812 <ayaanmkhan25192gmail.com>
2025-11-11 09:43:32 -0500

    control flow file added

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,51 @@
+let userRole = "admin";
+let accessLevel;
+let isLoggedIn = true;
+let userMessage;
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {     
+
+    case "admin":
+        userCategory = "administrator";      
+        break;
+
+    case "manager":
+        userCategory = "Manager";
+        break;
+        
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;               
+
+    default:
+        userCategory = "Unknown";
+}
+
+
+if (isLoggedIn) {
+    if (userRole === "admin") { 
+        userMessage = "welcomE admin!";
+        
+    } else {
+        userMessage = "welcome user!";
+
+    }
+} else {
+    userMessage = "please log in to acess the system.";
+    
+}
+
+if (userRole === "admin") {
+    accessLevel = "Full acess granted";         
+} 
+else if (userRole === "manager"){
+    accessLevel= "Limited access granted";    
+} else{
+    accessLevel = "No access granted";
+}
+
+console.log("Access Level: ", accessLevel);
+console.log("user Message:", userMessage);
+console.log("user Category:", userCategory);
